ea14d741b7ed40741b4ec6eb6a9d8546aff58e92
hannakathe2022 <hannakathe20@gmail.com>
2023-02-23 12:10:34 -0500

    Comentarios

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,20 +1,20 @@
 'use strict';
 
 function BinarioADecimal(num) {
-   var dec=0;
-   for(var i=0;num.length;i++){
-      dec+=num[i]*2**(num.length-1-i)
+   var dec=0; //Lleva la suma de los numeros decimales
+   for(var i=0;num.length;i++){ //Bucle que recorre la longitud del str
+      dec+=num[i]*2**(num.length-1-i) //Formula para obtener los decimales (1x2**n)
    }
    return dec;
 }
 
 function DecimalABinario(num) {
-   var bin='';
-   while(num>=1){
-      bin+=(num%2);
-      num=Math.floor(num/2);
+   var bin=''; //Srt vacio que almacena los numeros
+   while(num>=1){ //Bucle que lleva la condicion del numero minimo de cociente
+      bin+=(num%2); // Obtiene el residuo de la division 1 o 0
+      num=Math.floor(num/2); // Actualiza el valor de num
    }
-   return bin;
+   return bin.split().reverse().join();
 }
 
 console.log(BinarioADecimal('1100011'))
