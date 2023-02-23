46d85402798936bcf1bf0a4777931a1bc7a20fed
hannakathe2022 <hannakathe20@gmail.com>
2023-02-23 11:53:40 -0500

    Tarea Intro CS completa!

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,24 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   var dec=0;
+   for(var i=0;num.length;i++){
+      dec+=num[i]*2**(num.length-1-i)
+   }
+   return dec;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   var bin='';
+   while(num>=1){
+      bin+=(num%2);
+      num=Math.floor(num/2);
+   }
+   return bin;
+}
+
+console.log(BinarioADecimal('1100011'))
+console.log(DecimalABinario(99))
 
 module.exports = {
    BinarioADecimal,
